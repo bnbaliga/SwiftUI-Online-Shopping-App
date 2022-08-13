b198b4aa9190023a098f2b090f022dc91d1023d3
Narasimha Baliga <bnbaliga@gmail.com>
2022-08-13 12:19:31 -0500

    first commit

---
+++ OnlineShopping/OnlineShopping/OnlineShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShoppingApp.swift
+//  OnlineShopping
+//
+//  Created by Narasimha Baliga on 8/13/22.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
